ba4158ee0286edcc184a3e6e35ce367c4c550606
quanghuy-811 <anhthu157.do@gmail.com>
2024-12-13 23:35:29 +0700

    update full code

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -2,19 +2,31 @@ import { Button, Card, Modal, ModalHeader } from "flowbite-react";
 
 const ModalDetail = (props) => {
   const { dataShoe, closeModal } = props;
+  // console.log(dataShoe);
 
   return (
     <div>
       <Button>Toggle modal</Button>
-      <Modal show={!!dataShoe} onClose={closeModal}>
+      <Modal size="3xl" show={!!dataShoe} onClose={closeModal}>
         <Modal.Header>
           <h1 className="font-semibold text-2xl"> {dataShoe.name}</h1>
         </Modal.Header>
         <Modal.Body>
-          <Card className="max-w-sm" imgSrc={dataShoe.image} horizontal>
-            <p className="font-normal text-gray-700">{dataShoe.description}</p>
-            <p className="font-semibold text-xl">Price: {dataShoe.price}</p>
-          </Card>
+          <div className="flex items-center space-x-4">
+            <img
+              src={dataShoe.image}
+              alt="Example"
+              className="mb-4 rounded-lg w-2/5 border border-gray-200 shadow-md"
+            />
+            <div className="w-3/5">
+              <p className="font-normal text-gray-700">
+                {dataShoe.description}
+              </p>
+              <p className="font-semibold text-xl my-6">
+                Price: {dataShoe.price}
+              </p>
+            </div>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button className="ml-auto" color="gray" onClick={closeModal}>
--- src/Components/ShoeShop.jsx
+++ src/Components/ShoeShop.jsx
@@ -1,24 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { data } from "../../data.js";
 import ProductItem from "./ProductItem.jsx";
 import ModalDetail from "./Modal.jsx";
+import axios from "axios";
 
 const ShoeShop = () => {
-  const [shoe, setShoe] = useState(null);
-  const openModal = (product) => setShoe(product);
-  const closeModal = () => setShoe(null);
+  const [shoe, setShoe] = useState([]);
+  const [data, setData] = useState(null);
+  const openModal = (product) => setData(product);
+  const closeModal = () => setData(null);
 
+  const getAllProductAxios = () => {
+    axios({
+      url: "https://apistore.cybersoft.edu.vn/api/Product",
+      method: "GET",
+    })
+      .then((res) => {
+        setShoe(res.data.content);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getAllProductAxios();
+  }, []);
   return (
     <div className="container max-w-screen-xl mx-auto">
       <h1 className="text-3xl py-5 text-center">ShoeShope</h1>
 
       <div className="grid grid-cols-4 gap-5 ">
-        {data.map((item) => {
-          return <ProductItem data={item} openModal={openModal} />;
+        {shoe.map((item) => {
+          return (
+            <ProductItem key={item.id} data={item} openModal={openModal} />
+          );
         })}
       </div>
 
-      {shoe && <ModalDetail dataShoe={shoe} closeModal={closeModal} />}
+      {data && <ModalDetail dataShoe={data} closeModal={closeModal} />}
     </div>
   );
 };
